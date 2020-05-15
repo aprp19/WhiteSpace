bd03822ccff5f122393b43406ff521c27a097261
Angga P <kierska19@gmail.com>
2020-05-15 23:29:35 +0700

    New Update

--- app/src/main/java/com/aprp/whitespace/ui/profile/Profile.kt
+++ app/src/main/java/com/aprp/whitespace/ui/profile/Profile.kt
@@ -22,10 +22,6 @@ class Profile : Fragment() {
         profileViewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_profile)
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
